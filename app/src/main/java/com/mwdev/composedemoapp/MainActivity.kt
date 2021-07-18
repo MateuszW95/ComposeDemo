95af6bab4417923a3202ba319010e0e3cf156397
Mateusz Wieczorek <mwieczorek@i2asolutions.com>
2021-07-18 13:24:13 +0200

    Init commit

---
+++ app/src/main/java/com/mwdev/composedemoapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mwdev.composedemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mwdev.composedemoapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
